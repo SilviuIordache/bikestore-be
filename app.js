21d646c4e1f564a6419b784eaf67e04879c9dfc4
Silviu Iordache <silviu.iordache@caphyon.com>
2020-09-05 17:23:02 +0300

    Refactor the express app.

--- app.js
+++ app.js
@@ -4,29 +4,36 @@ const morgan = require('morgan');
 const app = express();
 const helmet = require('helmet');
 const multer = require('multer');
+const path = require('path');
 
 const bodyParser = require('body-parser')
 const bikeRouter = require('./routes/bikeRoutes');
-
-app.use(cors());
-app.use(helmet());
-
-
-const upload = multer({
-  dest: './uploads/'
-});
-
-console.log(`Running in ${process.env.NODE_ENV} mode`)
 
 // MIDDLEWEARS
 if (process.env.NODE_ENV === 'development') {
   // enable logging only for development mode
   app.use(morgan('dev'));
 } 
+
+// enable cross origin resource sharing
+app.use(cors());
+
+// enable security features
+app.use(helmet());
+
+// serve static folder from the server
+app.use('/static', express.static(path.join(__dirname, "static" )))
+
+console.log(`Running in ${process.env.NODE_ENV} mode`)
+
+
+
+// file upload middlewear
+const upload = multer({ dest: './uploads/'});
 app.use(upload.single('file'));
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
